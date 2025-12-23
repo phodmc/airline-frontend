3fecc56c2f6915320f696ee96367b397309c7178
Pho <115372840+phodmc@users.noreply.github.com>
2025-12-23 04:43:12 +0000

    status of user indicated on nav bar

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,56 @@
+<script setup>
+// Logic will go here later
+
+import { ref, onMounted, watch } from "vue";
+import { useRoute, useRouter } from "vue-router";
+
+const isLoggedIn = ref(false);
+const router = useRouter();
+const route = useRoute();
+
+const checkLogin = () => {
+    isLoggedIn.value = !!localStorage.getItem("token");
+};
+
+const logout = () => {
+    localStorage.removeItem("token");
+    isLoggedIn.value = false;
+    router.push("/login");
+};
+
+watch(() => route.path, checkLogin);
+onMounted(checkLogin);
+</script>
+
 <template>
     <div class="min-h-screen bg-gray-100">
         <header
             class="bg-blue-600 text-white p-4 shadow-md flex justify-between"
         >
-            <h1 class="text-2xl font-bold">Aries Airways</h1>
+            <RouterLink to="/" class="mx-2"
+                ><h1 class="text-2xl font-bold">Aries Airways</h1></RouterLink
+            >
             <nav>
                 <RouterLink to="/" class="mx-2 hover:underline"
                     >Search</RouterLink
                 >
-                <RouterLink to="/login" class="mx-2 hover:underline"
+                <RouterLink
+                    v-if="!isLoggedIn"
+                    to="/login"
+                    class="hover:underline"
                     >Login</RouterLink
                 >
+                <template v-else>
+                    <RouterLink to="/admin" class="mx-2 hover:underline"
+                        >Admin Panel</RouterLink
+                    >
+                    <button
+                        @click="logout"
+                        class="text-red-200 hover:text-white"
+                    >
+                        Logout
+                    </button>
+                </template>
             </nav>
         </header>
 
@@ -19,7 +59,3 @@
         </main>
     </div>
 </template>
-
-<script setup>
-// Logic will go here later
-</script>
