938ba3e8ce013d46bd6efbad1744df1d466f4b17
Pho <115372840+phodmc@users.noreply.github.com>
2025-12-23 04:41:14 +0000

    implement token validity checks before navigating to authenticated routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,28 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 import LoginView from "../views/LoginView.vue";
+import AdminView from "@/views/AdminView.vue";
 
 const routes = [
   { path: "/", component: HomeView },
   { path: "/login", component: LoginView },
+  { path: "/admin", component: AdminView, meta: { requiresAuth: true } },
 ];
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
+});
+
+// makes sure valid token is provided when accessing secured/authenticated routes
+router.beforeEach((to, from, next) => {
+  const isAuthenticated = !!localStorage.getItem("token");
+
+  if (to.meta.requiresAuth && !isAuthenticated) {
+    next("/login");
+  } else {
+    next();
+  }
 });
 
 export default router;
