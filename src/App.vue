2d63f835ebf651c38592a91402db12651614b1c4
Pho <115372840+phodmc@users.noreply.github.com>
2025-12-23 03:10:31 +0000

    fix: include routing logic for home and login view

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,21 @@
 <template>
     <div class="min-h-screen bg-gray-100">
-        <header class="bg-blue-600 text-white p-4 shadow-md">
+        <header
+            class="bg-orange-600 text-white p-4 shadow-md flex justify-between"
+        >
             <h1 class="text-2xl font-bold">Aries Airways</h1>
+            <nav>
+                <RouterLink to="/" class="mx-2 hover:underline"
+                    >Search</RouterLink
+                >
+                <RouterLink to="/login" class="mx-2 hover:underline"
+                    >Login</RouterLink
+                >
+            </nav>
         </header>
 
-        <main class="p-6">
-            <h2 class="text-xl">Welcome to the Cockpit</h2>
-            <p>The frontend is officially connected.</p>
+        <main>
+            <RouterView />
         </main>
     </div>
 </template>
