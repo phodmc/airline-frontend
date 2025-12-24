4021c3992d42dc3acdba6cd2fb7fee8c0d770e2f
Pho <115372840+phodmc@users.noreply.github.com>
2025-12-24 23:35:14 +0000

    Update api.js to include response interceptor

--- src/helpers/api.js
+++ src/helpers/api.js
@@ -20,4 +20,14 @@ api.interceptors.request.use(
   },
 );
 
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response && error.response.status === 401) {
+      localStorage.removeItem("token");
+      window.location.href = "/login";
+    }
+  },
+);
+
 export default api;
